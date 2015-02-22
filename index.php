4bc153ba00f00258c5ec9c2da20aae41fa535932
Hunter Fuller <hfuller@pixilic.com>
2015-02-22 16:14:30 -0600

    don't show stuff that isn't implemented (and may not ever be implemented within this app)

--- index.php
+++ index.php
@@ -9,13 +9,15 @@ echo($template["header"]);
 	<div class="col-sm-6">
 
 <h1>Your account</h1>
-<p>If it isn't a link, the functionality hasn't been written yet. Sorry about that.</p>
+<!-- <p>If it isn't a link, the functionality hasn't been written yet. Sorry about that.</p> -->
 
 <h3><a href="passwd.php">Change your password</a></h3>
-<h3><!--<a href="nfc.php">-->Register an NFC tag<!--</a>--></h3>
+<!--
+<h3><a href="nfc.php">Register an NFC tag</a></h3>
 <h3>Deregister an NFC tag</h3>
 <h3>Register a USB key</h3>
 <h3>Deregister a USB key</h3>
+-->
 <h3><a href="logout.php">Log out of this service</a></h3>
 
 	</div>
