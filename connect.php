7eb4cf69717a136b44c90b4c1b51599deeb30bc1
Hunter Fuller <hfuller@pixilic.com>
2015-03-17 21:43:15 -0500

    timeout isn't reliable

--- connect.php
+++ connect.php
@@ -7,7 +7,7 @@ if ($_REQUEST["type"] === "unlock") {
 		#we are not at the shop
 		die("You can't do this when you're not at the shop.");
 	}
-	$cmd = "timeout 6 ssh -tt makerbaker@doorpi \"sudo /home/pi/DoorAuth/DoorAuth1.7/src/CLI_Unlock.py " . escapeshellcmd($_SESSION["uid"]) . "\"";
+	$cmd = "timeout 10 ssh -tt makerbaker@doorpi \"sudo /home/pi/DoorAuth/DoorAuth1.7/src/CLI_Unlock.py " . escapeshellcmd($_SESSION["uid"]) . "\"";
 	$ret = exec($cmd);
 	if ( strlen($ret) > 0 ) {
 		header("Location: unlock_done.php?status=done&out=" . $ret);
