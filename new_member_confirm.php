acb77883b76952419dadf62e89d8ad6a4f49929f
Kinsey Moore <kmoore@digium.com>
2015-09-19 18:54:22 -0500

    Add new member confirmation frontend

---
+++ new_member_confirm.php
@@ -0,0 +1,25 @@
+<?php require_once("app.php");
+$auth = new Authenticator();
+$auth->requireBoardUser();
+echo($template["header"]);
+?>
+
+<div class="row"><div class="col-sm-2"></div><div class="col-sm-8">
+
+<div class="row">
+	<div>
+		<h1>End the Vote for a New Member Application</h1>
+	</div>
+</div>
+
+    <form class="form-horizontal" action="new_member_confirm_submit.php" method="POST">
+<?php
+generateFormInput("pending_member", "select", "Pending Member", getPendingMembers());
+generateFormInput("btn", "submit", "Make Active", "Confirm");
+?>
+    </form>
+
+</div>
+
+<?php echo($template["footer"]); ?>
+
